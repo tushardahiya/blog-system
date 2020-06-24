8b159f0191aea39620188f73a3b1186f43b15713
Tushar <tushardahiya84@gmail.com>
2020-06-24 21:08:14 +0530

    merged and integrated login routes and links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import SignUp from './containers/SignUp/SignUp'
 import Feed from './containers/Feed/Feed'
 import FullPost from './components/FullPost/FullPost'
-import {Switch} from 'react-router-dom'
-class App extends Component{
-  render(){
-    return(
+import Login from './components/Login/Login'
+import { Switch } from 'react-router-dom'
+class App extends Component {
+  render() {
+    return (
       <Layout>
         <Switch>
-          <Route path = "/sign-up" component={SignUp}/>
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/login" exact component={Login} />
           <Route path="/" exact component={Feed} />
           <Route path="/:id" exact component={FullPost} />
+
         </Switch>
       </Layout>
+    )
+  }
+}
 export default App;
--- src/components/UI/Toolbar/Toolbar.js
+++ src/components/UI/Toolbar/Toolbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import classes from './Toolbar.module.css'
 import Logo from '../Logo/Logo'
 import Aux from '../../../hoc/Auxiliary/Auxiliary'
 const toolbar = (props) => {
     return (
-    <Aux>
-        <div className={classes.NavBar}>
-            <Logo />
-            <h2><i>Views</i></h2>
-            <ul>
-                <li><NavLink to="/">Login</NavLink></li>
-                <li><NavLink to="/sign-up" >SignUp</NavLink></li>
-            </ul>
-        </div>
-    </Aux>
+        <Aux>
+            <div className={classes.NavBar}>
+                <Logo />
+                <h2><i>Views</i></h2>
+                <ul>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                    <li><NavLink to="/sign-up" >SignUp</NavLink></li>
+                </ul>
+            </div>
+        </Aux>
     )
 }
 export default toolbar;
